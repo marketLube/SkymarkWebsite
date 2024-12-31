a87b694e88edf99e4affbc3a3838149a1269c65f
marketLube <marketlubes@gmail.com>
2024-12-31 05:43:02 -0800

    meadia query in tab

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <div className="research-enrolled">1,11,11,131 Students Enrolled</div>
       <Main>
         <Home></Home>
-        {/* {isMob ? <AboutMob /> : <About></About>} */}
+        {isMob ? <AboutMob /> : <About></About>}
         <StudyIn></StudyIn>
         <ScrollCard></ScrollCard>
         <Testimonial></Testimonial>
