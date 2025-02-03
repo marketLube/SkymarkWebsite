7a2e9012444766c4f11c0e77a3b680692d23cbe9
marketLube <marketlubes@gmail.com>
2025-02-03 15:17:59 +0530

    sl

--- src/Components/EnquiryForm.jsx
+++ src/Components/EnquiryForm.jsx
@@ -12,25 +12,11 @@ function EnquiryForm() {
     <>
       <Header />
       <main className="enquary-content">
-        {isMobile ? <EnQueryMob /> : <EnquireSectionOne />}
+        <EnquireSectionOne />
         <EnquirySectionTwo />
-        <Footer />
       </main>
-      <div className="enquary-bottomDiv">
-        <img src={GlobeSec} alt="logo" className="enquary-respoImg" />
-        <div className="enquary-mobile-text">
-          <div style={{ width: "fit-content" }}>Start your</div>
-          <div style={{ width: "fit-content" }}>global career</div>
-          <div style={{ width: "fit-content" }}>today</div>
-        </div>
-        <img
-          src={Flags}
-          alt="flags"
-          className="enquary-flagRowRespo"
-          style={{ marginBottom: "2rem" }}
-        />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
