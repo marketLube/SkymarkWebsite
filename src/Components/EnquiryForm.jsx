08da4899891b69d0fa3a80bc328e06faceedab09
marketLube <marketlubes@gmail.com>
2025-02-03 18:38:15 +0530

    sl

--- src/Components/EnquiryForm.jsx
+++ src/Components/EnquiryForm.jsx
@@ -3,16 +3,19 @@ import EnquireSectionOne from "./EnquireSectionOne";
 import EnquirySectionTwo from "./EnquireSectionTwo";
 import Footer from "./Footer";
 import Header from "../layout/Header";
+import EnQueryMob from "./EnQueryMob";
 
 function EnquiryForm() {
   const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  console.log(isMobile, "isMobile");
 
   return (
     <>
       <Header />
       <main className="enquary-content">
         <EnquireSectionOne />
-        <EnquirySectionTwo />
+        {isMobile ? <EnQueryMob /> : <EnquirySectionTwo />}
       </main>
       <Footer />
     </>
