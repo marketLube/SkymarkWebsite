734dd7e2588f208d351251dbbcf5d20461624d39
market lube <marketlubes@gmail.com>
2025-02-18 22:00:21 +0530

    cout up set up

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,18 +1,49 @@
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 export default function Counter() {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: false,
+  });
+
   return (
-    <div className="counter">
+    <div className="counter" ref={ref}>
       <div className="counter-1">
-        <span>5000+</span>
+        <span>
+          <CountUp
+            end={5000}
+            duration={2.5}
+            suffix="+"
+            start={0}
+            enableScrollSpy
+          />
+        </span>
         <div>Admissions </div>
       </div>
       <div className="counter-1">
-        <span>1000+</span>
+        <span>
+          <CountUp
+            end={1000}
+            duration={2.5}
+            suffix="+"
+            start={0}
+            enableScrollSpy
+          />
+        </span>
         <div>Universities</div>
       </div>
       <div className="counter-1">
-        <span>250+</span>
+        <span>
+          <CountUp
+            end={250}
+            duration={2.5}
+            suffix="+"
+            start={0}
+            enableScrollSpy
+          />
+        </span>
         <div>Counsellors</div>
       </div>
     </div>
