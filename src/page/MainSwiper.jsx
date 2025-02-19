44c0e1193176f728b5cb1a9a42dd0925ebb70797
market lube <marketlubes@gmail.com>
2025-02-19 16:22:27 +0530

    change the yt slicker to swiper

--- src/page/MainSwiper.jsx
+++ src/page/MainSwiper.jsx
@@ -1,80 +1,12 @@
 import React, { useEffect } from "react";
-import Slider from "react-slick/lib/slider";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
 
 export default function MainSwiper() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 5000,
-    slidesToShow: 4,
-    autoplay: true,
-    autoplaySpeed: 0,
-    swipeToSlide: true,
-    cssEase: "linear",
-    centerMode: true,
-    arrows: false,
-    centerPadding: "20px",
-    pauseOnHover: true,
-    draggable: true,
-    swipe: true,
-
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 3,
-          centerMode: true,
-          speed: 500,
-          autoplay: false,
-        },
-      },
-      {
-        breakpoint: 960,
-        settings: {
-          slidesToShow: 2.5,
-          centerMode: true,
-          speed: 500,
-          autoplay: false,
-        },
-      },
-      {
-        breakpoint: 821,
-        settings: {
-          slidesToShow: 2,
-          centerMode: true,
-          speed: 500,
-          autoplay: false,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1.6,
-          centerMode: true,
-          speed: 500,
-          autoplay: false,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1.6,
-          centerMode: true,
-          speed: 500,
-          autoplay: false,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          centerMode: true,
-          speed: 500,
-          autoplay: false,
-        },
-      },
-    ],
-  };
+  const swiperRef = React.useRef(null);
 
   useEffect(() => {
     let players = [];
@@ -89,14 +21,32 @@ export default function MainSwiper() {
           .querySelectorAll(".youtube-player")
           .forEach((iframe, index) => {
             players[index] = new YT.Player(iframe, {
+              playerVars: {
+                autoplay: 0,
+                controls: 1,
+                rel: 0,
+                modestbranding: 1,
+              },
               events: {
+                onReady: (event) => {
+                  event.target.pauseVideo();
+                },
                 onStateChange: (event) => {
                   if (event.data === YT.PlayerState.PLAYING) {
+                    // Pause other videos
                     players.forEach((player, idx) => {
                       if (idx !== index && player.pauseVideo) {
                         player.pauseVideo();
                       }
                     });
+                    // Stop swiper autoplay
+                    swiperRef.current?.autoplay?.stop();
+                  } else if (
+                    event.data === YT.PlayerState.PAUSED ||
+                    event.data === YT.PlayerState.ENDED
+                  ) {
+                    // Resume swiper autoplay
+                    swiperRef.current?.autoplay?.start();
                   }
                 },
               },
@@ -111,82 +61,134 @@ export default function MainSwiper() {
   }, []);
 
   const isTab = window.innerWidth < 998;
+
   return (
     <div>
       <div className="main-swiper">
         <div className="main-swiper-container">
-          <Slider {...settings}>
-            <div className="swiper-body-item">
-              <span>
-                <iframe
-                  className="youtube-player"
-                  src="https://www.youtube.com/embed/uBDbrr6cjPE?enablejsapi=1"
-                  title="Frame 1"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  loading="lazy"
-                  allowFullScreen
-                />
-              </span>
-            </div>
-            <div className="swiper-body-item">
-              <span>
-                <iframe
-                  className="youtube-player"
-                  src="https://www.youtube.com/embed/unR5vDRuLj4?enablejsapi=1"
-                  title="Frame 2"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  loading="lazy"
-                  allowFullScreen
-                />
-              </span>
-            </div>
-            <div className="swiper-body-item">
-              <span>
-                <iframe
-                  className="youtube-player"
-                  src="https://www.youtube.com/embed/xcGU-1FB_kA?enablejsapi=1"
-                  title="Frame 3"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  loading="lazy"
-                  allowFullScreen
-                />
-              </span>
-            </div>
-            <div className="swiper-body-item">
-              <span>
-                <iframe
-                  className="youtube-player"
-                  src="https://www.youtube.com/embed/dGU52bhgI7M?enablejsapi=1"
-                  title="Frame 4"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  loading="lazy"
-                  allowFullScreen
-                />
-              </span>
-            </div>
-            <div className="swiper-body-item">
-              <span>
-                <iframe
-                  className="youtube-player"
-                  src="https://www.youtube.com/embed/BOuXCyMuVCs?enablejsapi=1"
-                  title="Frame 5"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  loading="lazy"
-                  allowFullScreen
-                />
-              </span>
-            </div>
-          </Slider>
+          <Swiper
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper;
+            }}
+            modules={[Autoplay]}
+            spaceBetween={20}
+            slidesPerView={4}
+            loop={true}
+            speed={5000}
+            autoplay={{
+              delay: 1500,
+              disableOnInteraction: true,
+              pauseOnMouseEnter: true,
+              stopOnLastSlide: false,
+            }}
+            breakpoints={{
+              1400: {
+                slidesPerView: 4,
+                speed: 1000,
+              },
+              960: {
+                slidesPerView: 2.5,
+                speed: 1000,
+              },
+              821: {
+                slidesPerView: 2,
+                speed: 1000,
+              },
+              768: {
+                slidesPerView: 1.6,
+                speed: 1000,
+              },
+              575: {
+                slidesPerView: 1.6,
+                speed: 1000,
+              },
+              500: {
+                slidesPerView: 1,
+                speed: 1000,
+              },
+            }}
+          >
+            <SwiperSlide>
+              <div className="swiper-body-item">
+                <span>
+                  <iframe
+                    className="youtube-player"
+                    src="https://www.youtube.com/embed/uBDbrr6cjPE?enablejsapi=1"
+                    title="Frame 1"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    loading="lazy"
+                    allowFullScreen
+                  />
+                </span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-body-item">
+                <span>
+                  <iframe
+                    className="youtube-player"
+                    src="https://www.youtube.com/embed/unR5vDRuLj4?enablejsapi=1"
+                    title="Frame 2"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    loading="lazy"
+                    allowFullScreen
+                  />
+                </span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-body-item">
+                <span>
+                  <iframe
+                    className="youtube-player"
+                    src="https://www.youtube.com/embed/xcGU-1FB_kA?enablejsapi=1"
+                    title="Frame 3"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    loading="lazy"
+                    allowFullScreen
+                  />
+                </span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-body-item">
+                <span>
+                  <iframe
+                    className="youtube-player"
+                    src="https://www.youtube.com/embed/dGU52bhgI7M?enablejsapi=1"
+                    title="Frame 4"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    loading="lazy"
+                    allowFullScreen
+                  />
+                </span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-body-item">
+                <span>
+                  <iframe
+                    className="youtube-player"
+                    src="https://www.youtube.com/embed/BOuXCyMuVCs?enablejsapi=1"
+                    title="Frame 5"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    loading="lazy"
+                    allowFullScreen
+                  />
+                </span>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
       {!isTab && (
